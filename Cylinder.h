d58c1bce1e5ffee39898a0ab087e11c7f421f5cf
Meng Yan <shaoran.cmy@hotmail.com>
2018-04-05 16:54:32 +0800

    added 2 function

--- Cylinder.h
+++ Cylinder.h
@@ -4,7 +4,18 @@
 #include <math.h>
 
 class Cylinder{
-
+private:
+	double radius;
+	double height;
+	
+public:
+	double totalArea(){
+		return radius * height;
+	}
+	
+	double volume(){
+		return 22.0 / 7 * radius * radius * height;
+	}
 };
 
 #endif
