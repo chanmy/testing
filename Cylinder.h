487686f2e185e2bd503e6754b07591bc45f9d9f5
Meng Yan <shaoran.cmy@hotmail.com>
2018-04-05 17:01:16 +0800

    added multiple functions

--- Cylinder.h
+++ Cylinder.h
@@ -4,7 +4,36 @@
 #include <math.h>
 
 class Cylinder{
-
+private:
+	double radius;
+	double height;
+	
+public:
+	Cylinder(){};
+	
+	Cylinder(double r){
+		radius = r;
+	}
+	
+	void setRadius(double r){
+		radius = r;
+	}
+	
+	double baseArea(){
+		return 22.0 / 7 * pow(radius, 2);
+	}
+	
+	double lateralArea(){
+		return 2 * 22.0 / 7 * radius * height;
+	}
+	
+	double totalArea(){
+		return 2 * baseArea() + lateralArea();
+	}
+	
+	double Volume(){
+		return 22.0 / 7 * pow(radius, 2) * height / 3;
+	}
 };
 
 #endif
